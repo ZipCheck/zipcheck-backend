a7005a7147a4a43ee8ccb21e53275b831e1dab61
songkc5671 <songkc5671@naver.com>
2025-11-28 14:56:59 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/ssafy/zipcheck/ZipCheckApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipCheckApplication.class, args);
+    }
+
+}
