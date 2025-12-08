8d4a87beaa6d8b3f25138ebd599dd39e32698ea4
Minkey99 <songkc5671@naver.com>
2025-12-08 20:30:09 +0900

    feat : 지도 API 개발
    MapController :
    searchHouseDeals : 지역별 필터링
    getSidoNames, getGugunNames, getDongNames
    : 프론트 엔드에서 다음 스텝으로 넘어가게 도와주는 함수들
    
    mapMapper : 클라이언트 필터 범위에 따른 검색 로직 작성
    시도로만 검색하면 반환값이 많아서 Pagination 적용
    
    SecurityConfig : /zipcheck/map/ 경로는 권한 허용

---
+++ src/main/java/com/ssafy/zipcheck/deals/service/MapServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ssafy.zipcheck.deals.service;
+
+import com.ssafy.zipcheck.deals.dto.MapDealResponse;
+import com.ssafy.zipcheck.deals.dto.MapSearchRequest;
+import com.ssafy.zipcheck.deals.mapper.MapMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MapServiceImpl implements MapService {
+
+    private final MapMapper mapMapper;
+
+    @Override
+    public List<MapDealResponse> searchHouseDeals(MapSearchRequest request) {
+        return mapMapper.searchHouseDeals(request);
+    }
+
+    @Override
+    public List<String> getSidoNames() {
+        return mapMapper.getSidoNames();
+    }
+
+    @Override
+    public List<String> getGugunNames(String sidoName) {
+        return mapMapper.getGugunNames(sidoName);
+    }
+
+    @Override
+    public List<String> getDongNames(String sidoName, String gugunName) {
+        return mapMapper.getDongNames(sidoName, gugunName);
+    }
+}
