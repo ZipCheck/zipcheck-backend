f8ddfde9d94eaf82ddbfe318320ab448e578cd31
Minkey99 <songkc5671@naver.com>
2025-12-21 23:34:27 +0900

    feat : 줌인 줌아웃 매물 표시

--- src/main/java/com/ssafy/zipcheck/deals/mapper/MapMapper.java
+++ src/main/java/com/ssafy/zipcheck/deals/mapper/MapMapper.java
@@ -1,5 +1,6 @@
 package com.ssafy.zipcheck.deals.mapper;
 
+import com.ssafy.zipcheck.deals.dto.MapClusterResponse;
 import com.ssafy.zipcheck.deals.dto.MapDealResponse;
 import com.ssafy.zipcheck.deals.dto.MapSearchRequest;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,6 +10,8 @@ import java.util.List;
 @Mapper
 public interface MapMapper {
     List<MapDealResponse> searchHouseDeals(MapSearchRequest request);
+    List<MapClusterResponse> searchHouseClusters(MapSearchRequest request);
+    Long countSearchHouseDeals(MapSearchRequest request);
     MapDealResponse getDealById(long id);
     List<String> getSidoNames();
     List<String> getGugunNames(String sidoName);
